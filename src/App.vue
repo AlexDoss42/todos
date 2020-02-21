39f8fe055aabfc5ba63bb52e8c073674fcffe8e8
Alex Doss <alex.doss42@gmail.com>
2020-02-20 22:02:43 -0700

    adds delete functionality

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,12 @@ export default {
   },
   methods: {
       deleteTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id); 
+
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+          // eslint-disable-next-line no-unused-vars
+          .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+          .catch(err => console.log(err));
+         
       },
       addTodo(newTodo) {
         const { title, completed } = newTodo;
